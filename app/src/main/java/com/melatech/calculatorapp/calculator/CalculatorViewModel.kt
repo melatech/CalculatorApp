cd655563b279b8b7efca74102bee2aea894e747f
melatech <maincontractor@gmail.com>
2023-04-14 22:59:14 +0100

    created logic for enter number function ->restrict the number of characters displayed

--- app/src/main/java/com/melatech/calculatorapp/calculator/CalculatorViewModel.kt
+++ app/src/main/java/com/melatech/calculatorapp/calculator/CalculatorViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.*
 
-class CalculatorViewModel: ViewModel() {
+class CalculatorViewModel : ViewModel() {
 
     private val _state1: MutableStateFlow<String> = MutableStateFlow("")
     val state1: StateFlow<String> = _state1.asStateFlow()
@@ -18,112 +18,87 @@ class CalculatorViewModel: ViewModel() {
     private val _result: MutableStateFlow<String> = MutableStateFlow("")
     val result: StateFlow<String> = _result.asStateFlow()
 
-    fun enterNumber(number: String){
-        println("jason enterNumber: $number")
-       // val newDisplay = _state1.value + number
-
-        if (_operator.value == null) {
-            println("jason if operator is null block: ${_operator.value}")
-
-            //check if operator is empty
-            if (_state1.value.length >= MAX_NUM_LENGTH) {
-                println("jason if state1 is Max Length: ${_state1.value} ")
-                //if it is  empty we can append more numbers until the cut of point of 8
-                return
-            }
-        }
-        //check if state1 is empty and assign result to _state1.value
-        _state1.value = if (_state1.value == ""){
-            //if it is empty return the parameter value
-            "$number"
-        } else {
-            //if there is something in the state return the state with the previous multiple values
-            //and the parameter value at the end
-
-            "${_state1.value} $number"
-        }
-       if (_operator.value != null){
-           if (_state2.value == ""){
-               "$number"
-           } else {
-               "${_state2.value} $number"
-           }
-        }
-    }
-
-//    fun enterNumber(number: String){
-//        if (_operator.value == null){
-//            if (_state1.value.length >= MAX_NUM_LENGTH ){
-//                return
-//            }
-//        }
-//        _state1.value = if (_state1.value == ""){
-//            "$number"
-//        } else {
-//            "${_state1.value} $number"
-//        }
-//    }
-
-    fun performOperation(symbol: String){
-        println("jason performOperation")
-        if(_state1.value.isNotBlank()){
-            _operator.value = symbol
-        }
-    }
-
-    fun performCalculation(){
-        println("jason performCalculation")
-        combine(_state1, _state2, _operator){
-                s1, s2, op ->
-            println("jason performCalculation:$s1--$s2--$op")
+    init {
+        println("jason init")
+        combine(_state1, _operator, _state2) { s1, op, s2 ->
+            println("jason performCalculation2:$s1--$s2--$op")
 
             val number1 = s1.toInt()
             val number2 = s2.toInt()
+            val operator1 = op
+            println("jason performCalculation1:number1:$number1--number2:$number2")
 
-             when(op){
-                "+" -> {number1 + number2}
-                "-" -> {number1 - number2}
-                "*" -> {number1 * number2}
-                "/" -> {number1 / number2}
+            val result = when (operator1) {
+                "+" -> {
+                    number1 + number2
+                    println("jason performCalculation3:$s1--$s2--$op")
+                }
+                "-" -> {
+                    number1 - number2
+                }
+                "*" -> {
+                    number1 * number2
+                }
+                "/" -> {
+                    number1 / number2
+                }
                 else -> 0
             }
-            //_result.value = result.toString() ?: ""
-        }.onEach { value ->
-            _result.value = value.toString()
-        }.launchIn(viewModelScope)
+            _result.value = result.toString() ?: ""
+        }
+            .catch { e ->
+                e.message
+            }
+            .launchIn(viewModelScope)
     }
 
-//    fun performCalculation(){
-//        println("jason performCalculation")
-//        combine(_state1, _state2, _operator){
-//            state1, state2, operator1 ->
-//            Triple(state1, state2, operator1)
-//
-//            println("jason perfo")
-//
-//            val number1 = state1.toInt()
-//            val number2 = state2.toInt()
-//
-//         val result = when(operator1){
-//                "+" -> {number1 + number2}
-//                "-" -> {number1 - number2}
-//                "*" -> {number1 * number2}
-//                "/" -> {number1 / number2}
-//          else -> 0
-//            }
-//            _result.value = result.toString() ?: ""
-//        }
-//    }
+    fun enterNumber(number: String) {
+        println("jason enterNumber: $number")
+        println("jason _state1.length: ${_state1.value.length}")
+        println("jason _state2.length: ${_state2.value.length}")
 
-    fun performClearScreen(){
+        if (_operator.value == null) {
+            println("jason enterNumber: _operator:${_operator.value}")
+            if (_state1.value.length >= MAX_NUM_LENGTH) {
+                //println("jason enterNumber: _state1.length:${_state1.value.length}")
+                return
+            }
+            _state1.value = if (_state1.value == "") {
+                "$number"
+            } else {
+                //"${_state1.value} "
+                "${_state1.value}" + "$number"
+            }
+            return
+        }
+        if (_state2.value.length >= MAX_NUM_LENGTH) {
+            return
+        }
+        _state2.value = if (_state2.value == "") {
+            "$number"
+        } else {
+            "${_state1.value}" + " " + "${_operator.value}" + " " + "${_state2.value}" + "$number"
+        }
+    }
+
+    fun performOperation(symbol: String) {
+        println("jason performOperation: $symbol")
+        if (_state1.value.isNotBlank()) {
+            _operator.value = symbol
+            _result.value = "${_state1.value}" + " " + "$symbol" + " "
+        }
+    }
+
+    fun performCalculation() = _result.value
+
+    fun performClearScreen() {
         println("jason performDelete")
         _state1.value = ""
         _state2.value = ""
         _operator.value = ""
     }
 
-companion object {
-    private const val MAX_NUM_LENGTH = 9
-}
-
+    companion object {
+        private const val MAX_NUM_LENGTH = 8
+    }
 }
