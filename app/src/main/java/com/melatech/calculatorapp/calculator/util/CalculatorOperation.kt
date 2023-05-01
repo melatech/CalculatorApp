39f73f2b099e48d150f9f15b3a4eecd3bd2464a9
melatech <maincontractor@gmail.com>
2023-05-01 23:42:06 +0100

    Created addition and multiplication unit tests

--- app/src/main/java/com/melatech/calculatorapp/calculator/util/CalculatorOperation.kt
+++ app/src/main/java/com/melatech/calculatorapp/calculator/util/CalculatorOperation.kt
@@ -1,6 +1,6 @@
 package com.melatech.calculatorapp.calculator.util
 
-sealed class CalculatorOperation(symbol: String) {
+sealed class CalculatorOperation(val symbol: String) {
     object Add: CalculatorOperation("+")
     object Subtract: CalculatorOperation("-")
     object Multiply: CalculatorOperation("*")
---
+++ app/src/test/java/com/melatech/calculatorapp/calculator/CalculatorViewModelTest.kt
@@ -0,0 +1,87 @@
+package com.melatech.calculatorapp.calculator
+
+import com.melatech.calculatorapp.calculator.util.CalculatorAction
+import com.melatech.calculatorapp.calculator.util.CalculatorOperation
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class CalculatorViewModelTest {
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+    private lateinit var viewmodel: CalculatorViewModel
+
+    @Before
+    fun setUp() {
+        viewmodel = CalculatorViewModel()
+    }
+
+    @Test
+    fun `viewModel should add two numbers and return the correct result`() = runBlocking{
+
+        //mimic the first number button click to update operand1 stateflow with integer 4
+        viewmodel.onAction(CalculatorAction.Number(4))
+
+        //check that operand 1 stateflow has been updated the with integer 4
+        val actualOperand1 = viewmodel.state.value.operand1
+        assertEquals("4", actualOperand1)
+
+        //mimic the operator button click to update the operator stateflow with the string symbol "+"
+        viewmodel.onAction(CalculatorAction.Operation(CalculatorOperation.Add))
+
+        //check that operator stateflow has been updated with with the string symbol "+"
+        val actualOperator = viewmodel.state.value.operator?.symbol
+        assertEquals("+", actualOperator)
+
+        //mimic the second number button click to update operand2 stateflow with the integer 2
+        viewmodel.onAction(CalculatorAction.Number(2))
+
+        //check that operand 2 stateflow has been updated with the integer 2
+        val actualOperand2 = viewmodel.state.value.operand2
+        assertEquals("2", actualOperand2)
+
+        //mimic the equals button click to update operand1 stateflow with the correct result
+        viewmodel.onAction(CalculatorAction.Calculate)
+
+        //check that operand 1 stateflow has been updated with the correct result
+        val actualResult = viewmodel.state.value.operand1
+        assertEquals("6.0", actualResult)
+    }
+
+    @Test
+    fun `viewModel should Multiply two numbers and return the correct result`() = runBlocking{
+
+        //mimic the first number button click to update operand1 stateflow with integer 4
+        viewmodel.onAction(CalculatorAction.Number(4))
+
+        //check that operand 1 stateflow has been updated the with integer 4
+        val actualOperand1 = viewmodel.state.value.operand1
+        assertEquals("4", actualOperand1)
+
+        //mimic the operator button click to update the operator stateflow with the string symbol "*"
+        viewmodel.onAction(CalculatorAction.Operation(CalculatorOperation.Multiply))
+
+        //check that operator stateflow has been updated with with the string symbol "*"
+        val actualOperator = viewmodel.state.value.operator?.symbol
+        assertEquals("*", actualOperator)
+
+        //mimic the second number button click to update operand2 stateflow with the integer 4
+        viewmodel.onAction(CalculatorAction.Number(4))
+
+        //check that operand 2 stateflow has been updated with the integer 4
+        val actualOperand2 = viewmodel.state.value.operand2
+        assertEquals("4", actualOperand2)
+
+        //mimic the equals button click to update operand1 stateflow with the correct result
+        viewmodel.onAction(CalculatorAction.Calculate)
+
+        //check that operand 1 stateflow has been updated with the correct result
+        val actualResult = viewmodel.state.value.operand1
+        assertEquals("16.0", actualResult)
+    }
+}
